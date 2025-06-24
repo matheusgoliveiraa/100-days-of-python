efe92aba9f8b22d6831bea135630f387d4f28098
Matheus Oliveira <matheus.goliveira87@hotmail.com>
2025-06-24 19:02:10 -0300

    add file

---
+++ day01/main.py
@@ -0,0 +1,6 @@
+print('Welcome to the Band Name Generator.')
+
+name_city = input(('Which city did you grow up in: '))
+name_pet = input(('What is the name of a pet: '))
+
+print('Your band name could be: ' + name_city + " " + name_pet)
